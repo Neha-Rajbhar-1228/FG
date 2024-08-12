60aeb008811836194b309148b28c7b879b54844a
Neha-Rajbhar-1228 <rajrajbhar682@gmail.com>
2024-08-12 12:42:06 +0530

    changes

--- src/components/home/upcomingBatches/UpcomingBatchCard.js
+++ src/components/home/upcomingBatches/UpcomingBatchCard.js
@@ -7,7 +7,7 @@ function UpcomingBatchCard() {
       <div className="cardImgOutline">
         <img
           style={{ width: "100%" }}
-          alt="No Image"
+          alt="No"
           src="https://s3-alpha-sig.figma.com/img/21ba/48b9/2ece4ee7fa76f5db06eeeea635bbdad2?Expires=1724025600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=c7mxX5GwqFhjYanDKFrm~kk44SZYxLXyZRLAkq8OJ1RA14efmF4VkNLSga0HZY6sdBIcFRHZOq8Cmd6iAhP0Hlh28xpDXh3E20bdZGKWnVvK5armaQZXO7Xo0xYBuAxZN0FqsIofTtBzOqQjE2Aw~O~QVv7mGYfbQ~RmylrFq-qOAkFi0TmIDWRNB8ZeP6o7HXQlftza0PhdIfva-IRFLnXRslSlRdXk5TYXDK9DOETdjjAEZYkjLqpa9Bo9LTiHCvDp-rlle~XYNn3K9cilukrq30XfXcVSrEtAsU6IxzY2611rNWFR8-eSjmYQYtwG4EL6rmseYXQkLfFgbcWdVA__"
         />
       </div>
@@ -26,7 +26,7 @@ function UpcomingBatchCard() {
         </div>
         <div className="light_red_background">
           <div className="font_12_regular" style={{ color: "#C00000" }}>
-            <span> <img alt="No Image" style={{maxWidth:"7px",maxHeight:"7px",objectFit:"contain", marginRight:"4px"}} src="Ellipse437.png"/></span>Live
+            <span> <img alt="No" style={{maxWidth:"7px",maxHeight:"7px",objectFit:"contain", marginRight:"4px"}} src="Ellipse437.png"/></span>Live
           </div>
         </div>
       </div>
